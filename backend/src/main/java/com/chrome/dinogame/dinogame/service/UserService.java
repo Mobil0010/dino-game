064acd196e80b5e4d45ac1e6995969bc5a249ba6
khm14 <khm14978@gmail.com>
2025-05-27 13:13:29 +0900

    회원가입 중복 확인

--- backend/src/main/java/com/chrome/dinogame/dinogame/service/UserService.java
+++ backend/src/main/java/com/chrome/dinogame/dinogame/service/UserService.java
@@ -1,11 +1,12 @@
 package com.chrome.dinogame.dinogame.service;
 
-import com.chrome.dinogame.dinogame.entity.User;
-import com.chrome.dinogame.dinogame.repository.UserRepository;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import com.chrome.dinogame.dinogame.entity.User;
+import com.chrome.dinogame.dinogame.repository.UserRepository;
 
 @Service
 public class UserService {
@@ -14,12 +15,6 @@ public class UserService {
     private UserRepository userRepository;
 
     public User register(User user) {
-        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
-        throw new RuntimeException("이미 존재하는 아이디입니다.");
-        }
-        if (userRepository.findByNickname(user.getNickname()).isPresent()) {
-            throw new RuntimeException("이미 존재하는 닉네임입니다.");
-        }
         return userRepository.save(user);
     }
 
